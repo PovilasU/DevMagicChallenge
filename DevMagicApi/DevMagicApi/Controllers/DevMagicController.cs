adf84b534b7e0ff8498270b554fcad0d8f5f28a4
PovilasU <povilas.urbonas85@gmail.com>
2023-11-25 19:42:15 +0000

    Create DevMagicAPI Add the DevMagic Model to the API Add the DevMagic Controller

---
+++ DevMagicApi/DevMagicApi/Controllers/DevMagicController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevMagicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevMagicController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<DevMagic>>>GetEmployees()
+        {
+            return new List<DevMagic> {
+                new DevMagic
+                {
+                    EmployeeID = 3,
+                    ClientID = 2,
+                    OfficeID = 3,
+                    Name = "John Fisher",
+                    Bio = "Flowers are great!",
+                    DateOfBirth = "2001-02-11",
+
+                }
+            };
+        }
+    }
+}
+
+
---
+++ DevMagicApi/DevMagicApi/DevMagic.cs
@@ -0,0 +1,12 @@
+﻿namespace DevMagicApi
+{
+    public class DevMagic
+    {
+        public int EmployeeID { get; set; }
+        public int ClientID { get; set; }
+        public int OfficeID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Bio { get; set; } = string.Empty;
+        public string DateOfBirth { get; set; } = string.Empty;
+    }
+}
