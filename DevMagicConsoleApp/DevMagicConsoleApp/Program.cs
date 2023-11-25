ffc591edf09f73f9ca7f8e9a5d01fb41301c3e80
PovilasU <povilas.urbonas85@gmail.com>
2023-11-25 17:10:16 +0000

    Testing dummy extension to convert string

---
+++ DevMagicConsoleApp/DevMagicConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMagicConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "THIS IS TEst striG to test dummy extension";
+            //Console.WriteLine("Firts C# program Povilas Test".ToLower());
+            String result = str.GetString();
+            Console.WriteLine("Dummy results: " + result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DevMagicConsoleApp/DevMagicConsoleApp/TranslateDevMagic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMagicConsoleApp
+{
+    static class TranslateDevMagic
+    {
+        public static String GetString(this String word)
+        {
+            String result = word;
+
+            return result.ToLower();
+        }
+    }
+}
